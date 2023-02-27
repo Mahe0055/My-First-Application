811cd0e1e74c3f1d14785622035a00194fe2ebc0
Mahe0055 <mahe00555@stud.kea.dk>
2023-02-27 13:58:50 +0100

    Commit

--- app/src/main/java/com/example/my_first_application/MainActivity.java
+++ app/src/main/java/com/example/my_first_application/MainActivity.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    //String LOG_TAG = this.class
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Welcome to my first application");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
